649c7ada44cd54a6c8561127264629328cc0d818
Ria Wadhwa <114467791+riawadhwa@users.noreply.github.com>
2024-08-05 05:17:10 +0000

    adding file retrieve route

--- routes/admin_routes.js
+++ routes/admin_routes.js
@@ -1,10 +1,16 @@
 const route = require("express").Router();
 const UserController = require("../controller/user_controller");
-
+const { listUserDocumentsController, getFilesController } = require('../controller/viewfile_controller');
 
 route.post("/create-user", UserController.register);
 
 route.get("/users", UserController.getAllUsers);
+
+// Route to list user documents
+route.get('/files/user/:userId', listUserDocumentsController);
+
+// Route to retrieve a specific file
+route.get('/files/:id', getFilesController);
 
 
 module.exports = route;
